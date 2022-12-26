7cf9802ccf82011ad1710e665638a92ed32c2738
Amit <amitayare.hkb@gmail.com>
2022-12-26 14:47:18 +0530

    f

--- undo.sh
+++ undo.sh
@@ -2,9 +2,9 @@
 
 git pull origin master
 
-find /var/www/html/wl2/dev/ -name "*.env" -exec sed -i 's/APP_ENV=DEV/APP_ENV=LOCAL/g' {} \;
+find /mnt/Test/dev/ -name "*.env" -exec sed -i 's/APP_ENV=DEV/APP_ENV=LOCAL/g' {} \;
 
-find /var/www/html/wl2/testing/ -name "*.env" -exec sed -i 's/APP_ENV=TEST/APP_ENV=LOCAL/g' {} \;
+find /mnt/Test/testing/ -name "*.env" -exec sed -i 's/APP_ENV=TEST/APP_ENV=LOCAL/g' {} \;
 
 
 
