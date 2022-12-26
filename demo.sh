74c5817f1df3b05049912359459fa04b0d65d3ff
Amit <amitayare.hkb@gmail.com>
2022-12-26 12:29:49 +0530

    n

--- demo.sh
+++ demo.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-find /mnt/Test/var/ -name "*.txt" -exec sed -i 's/DEV=LOCAL/DEV=DEV/g' {} \; && find /mnt/Test/tar -name "*.txt" -exec sed -i 's/TEST=LOCAL/TEST=TEST/g' {} \;
+#find /mnt/Test/var/ -name "*.txt" -exec sed -i 's/DEV=LOCAL/DEV=DEV/g' {} \; && find /mnt/Test/tar -name "*.txt" -exec sed -i 's/TEST=LOCAL/TEST=TEST/g' {} \;
+
+if ./demo.sh | grep -q 'Already exist'; then
+   echo "pwd"
+fi
 
 
