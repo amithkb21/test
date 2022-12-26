3eca498a46822345cd60b031b4f6e7216c10c026
Amit <amitayare.hkb@gmail.com>
2022-12-26 14:29:02 +0530

    g

--- demo.sh
+++ demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#find /mnt/Test/var/ -name "*.txt" -exec sed -i 's/DEV=LOCAL/DEV=DEV/g' {} \; && find /mnt/Test/tar -name "*.txt" -exec sed -i 's/TEST=LOCAL/TEST=TEST/g' {} \;
+find /mnt/Test/var/ -name "*.txt" -exec sed -i 's/DEV=LOCAL/DEV=DEV/g' {} \; && find /mnt/Test/tar -name "*.txt" -exec sed -i 's/TEST=LOCAL/TEST=TEST/g' {} \;
 
 if git pull origin master | grep -q 'Already up to date'; then
    	echo "NOTHING TO CHANGE"
