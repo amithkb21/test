087601a5bd260b76d8a35517b1a46e8c74bde2f0
amithkb21 <121281179+amithkb21@users.noreply.github.com>
2022-12-26 15:50:32 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -3,5 +3,8 @@
 if git pull origin master | grep -q 'Already up to date'; then
 	echo "NOTHING TO CHANGE"
 else
-   echo "None of the condition met"
+   cd /mnt/Test/dev/
+   git diff origin/master
+   
+   
 fi
