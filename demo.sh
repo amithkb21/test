5987934d210be573d4b4adefde1230739930c2c1
amithkb21 <121281179+amithkb21@users.noreply.github.com>
2022-12-26 15:42:11 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -3,8 +3,6 @@
 if git pull origin master | grep -q 'Already up to date'; then
 	echo "NOTHING TO CHANGE"
 elif git diff origin/master; then
-	#find /mnt/Test/dev -name "*.env" -exec sed -i 's/LOCAL/DEV/g' {} \;
-	#find /mnt/Test/testing -name "*.env" -exec sed -i 's/LOCAL/TEST/g' {} \;
 else
    echo "None of the condition met"
 fi
