336ba2267553c380600362d16204a369c76b53f3
amithkb21 <121281179+amithkb21@users.noreply.github.com>
2022-12-26 14:32:16 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-find /mnt/Test/var/ -name "*.txt" -exec sed -i 's/DEV=LOCAL/DEV=DEV/g' {} \; && find /mnt/Test/tar -name "*.txt" -exec sed -i 's/TEST=LOCAL/TEST=TEST/g' {} \;
 
 if git pull origin master | grep -q 'Already up to date'; then
    	echo "NOTHING TO CHANGE"
 else
+	find /mnt/Test/dev -name "*.txt" -exec sed -i 's/DEV=LOCAL/DEV=DEV/g' {} \; && find /mnt/Test/testing -name "*.txt" -exec sed -i 's/TEST=LOCAL/TEST=TEST/g' {} \;
 	cd /mnt/Test/dev/
-	diff='git diff .evn'
+	git diff origin master
 fi
 
 
