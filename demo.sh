78caac303aed17222f5fb4e4e9abfebdd53f4cb7
Amit <amitayare.hkb@gmail.com>
2022-12-26 15:21:28 +0530

    test

--- demo.sh
+++ demo.sh
@@ -4,10 +4,12 @@ path=cd /mnt/Test/dev
 
 if [ git pull origin master | grep -q 'Already up to date' ] then
    	echo "NOTHING TO CHANGE"
-elif [ path git diff origin/master | grep -q 'LOCAl' ] then
+elif [ $path git diff origin/master | grep -q 'LOCAl' ] then
 	find /mnt/Test/dev -name "*.env" -exec sed -i 's/LOCAL/DEV/g' {} \;
 	find /mnt/Test/testing -name "*.env" -exec sed -i 's/LOCAL/TEST/g' {} \;
-	
+
+else 
+	echo " do nothing "
 	
 fi
 
