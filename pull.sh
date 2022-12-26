85198c862037e8fd9d46cf4caaca5935845301dc
Amit <amitayare.hkb@gmail.com>
2022-12-26 12:44:47 +0530

    add

--- pull.sh
+++ pull.sh
@@ -2,10 +2,5 @@
 
 #find /mnt/Test/var/ -name "*.txt" -exec sed -i 's/DEV=LOCAL/DEV=DEV/g' {} \; && find /mnt/Test/tar -name "*.txt" -exec sed -i 's/TEST=LOCAL/TEST=TEST/g' {} \;
 
-if git pull origin master | grep -q 'Already up to date'; then
-   pwd 
-else
-	echo "chnages happen"
-fi
-
+git pull origin master
 
