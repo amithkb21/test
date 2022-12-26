dbcda3a30743678bbfa8211dfbfe642e2c0471aa
amithkb21 <121281179+amithkb21@users.noreply.github.com>
2022-12-26 14:42:23 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -4,8 +4,7 @@
 if git pull origin master | grep -q 'Already up to date'; then
    	echo "NOTHING TO CHANGE"
 else
-	find /mnt/Test/dev -name "*.env" -exec sed -i 's/APP_ENV=LOCAL/APP_ENV=DEV/g' {} \;
-	find /mnt/Test/testing -name "*.env" -exec sed -i 's/s/APP_ENV=LOCAL/APP_ENV=TEST/g' {} \;
+	find /var/www/html/wl2/dev/ -name "*.env" -exec sed -i 's/APP_ENV=LOCAL/APP_ENV=DEV/g' {} \;
 	
 	cd /mnt/Test/dev/
 	git diff origin/master
