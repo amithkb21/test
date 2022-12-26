895257f69c71a0624c8e8d337b0c572dc2e069df
amithkb21 <121281179+amithkb21@users.noreply.github.com>
2022-12-26 14:54:59 +0530

    Update undo.sh

--- undo.sh
+++ undo.sh
@@ -2,9 +2,9 @@
 
 git pull origin master
 
-find /mnt/Test/dev/ -name "*.env" -exec sed -i 's/APP_ENV=DEV/APP_ENV=LOCAL/g' {} \;
+find /mnt/Test/dev/ -name "*.env" -exec sed -i 's/DEV/LOCAL/g' {} \;
 
-find /mnt/Test/testing/ -name "*.env" -exec sed -i 's/APP_ENV=TEST/APP_ENV=LOCAL/g' {} \;
+find /mnt/Test/testing/ -name "*.env" -exec sed -i 's/TEST/LOCAL/g' {} \;
 
 
 
