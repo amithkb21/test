0b74b3134a69164aeafd793c025cb42a19019bc2
amithkb21 <121281179+amithkb21@users.noreply.github.com>
2022-12-27 10:03:08 +0530

    old

--- replace.sh
+++ replace.sh
@@ -7,10 +7,15 @@ commit_id=$(git log -n 1 --pretty=format:%H "$origin/$branch")
 echo $commit_id 
 git pull origin master 
 
-commit_old=$(git log -n 2 --pretty=format:%H "$origin/$branch" | tail -1 )
+commit_old=$(git log -n 2 --pretty=format:%H "$origin/$branch")
 echo $commit_old
 
+if [ $commit_id == $commit_old ];
 
+then 
+	echo " No new commits "
+else
+	echo " new commit id found "
 
-
+fi
 
