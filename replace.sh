a4329c4215d0611500593086ed5728d7dde6f0a8
amithkb21 <121281179+amithkb21@users.noreply.github.com>
2022-12-27 10:31:26 +0530

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -9,7 +9,8 @@ git pull origin master
 
 commit_old=$(git log -n 1 --pretty=format:%H "$origin/$branch")
 echo $commit_old > /home/amitayare/file1.txt
-
+value='cat /home/amitayare/file1.txt'  
+echo "$value"  
 if [ $commit_id == $commit_old ];
 
 then 
